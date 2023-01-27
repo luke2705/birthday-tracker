82f14b6efac318c0402b0eecf4f2b0514b42699a
luke.west <luke.west@frogslayer>
2023-01-26 22:01:51 -0600

    added copyright footer

--- web/birthday-tracker-app/src/App.tsx
+++ web/birthday-tracker-app/src/App.tsx
@@ -13,21 +13,32 @@ const MockDataRibbon = styled.span`
     transform: rotate(-45deg);
 `;
 
+const CopyrightText = styled.span`
+    color: #adb5bd;
+    margin-bottom: 10px;
+`;
+
 
 function App() {
   const useMockData = true;
 
   return (
       <div className="App">
-          <header className="App-header">
-              { useMockData &&
-                  <MockDataRibbon title={'Consult README for more info'}>
-                      Using Mock Data
-                  </MockDataRibbon>
-              }
-              <h1>Birthday Tracker</h1>
-          </header>
-          <MainBirthdayPage useMockData={useMockData}/>
+          <div>
+              <header className="App-header">
+                  { useMockData &&
+                      <MockDataRibbon title={'Consult README for more info'}>
+                          Using Mock Data
+                      </MockDataRibbon>
+                  }
+                  <h1>Birthday Tracker</h1>
+              </header>
+              {/* add more routes here as your empire grows */}
+              <MainBirthdayPage useMockData={useMockData}/>
+          </div>
+          <CopyrightText>
+              © 2023 Luke West. All rights reserved.
+          </CopyrightText>
       </div>
   );
 }
