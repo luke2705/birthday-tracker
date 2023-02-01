162cccd84dcd7b5910072d9c10df027d3bb4607e
luke.west <luke.west@frogslayer>
2023-01-31 18:38:18 -0600

    adding default nulls to reminders table

--- web/birthday-tracker-app/src/components/reminders-page/birthday-table-row.tsx
+++ web/birthday-tracker-app/src/components/reminders-page/birthday-table-row.tsx
@@ -2,6 +2,7 @@ import { StyledSelect, TableData, TableRow } from './reminders-page.styles';
 import { add, format, parseISO } from 'date-fns';
 import { Checkbox, MenuItem, SelectChangeEvent } from '@mui/material';
 import React from 'react';
+import { Birthday } from '../../types/birthday';
 
 const BirthdayTableRow = (props: any) => {
     const birthdayInfo = props.birthdayInfo;
@@ -18,12 +19,25 @@ const BirthdayTableRow = (props: any) => {
         props.handleBirthdayUpdate(updatedBirthday);
     }
 
+    function getDefaultReminderInterval(birthday: Birthday) {
+        if (birthday.precedingDaysForReminder == null
+            || birthday.reminderEnabled == false
+            || birthday.reminderEnabled == null) {
+            return -1;
+        }
+        return birthday.precedingDaysForReminder;
+    }
+
     return (
         <TableRow key={birthdayInfo.name}>
             <TableData>{birthdayInfo.name}</TableData>
             <TableData>{format(birthday, 'MMM d')}</TableData>
             <TableData>
-                <StyledSelect onChange={(event) => handlePrecedingDayChange(event)} defaultValue={birthdayInfo.precedingDaysForReminder == null ? 1 : birthdayInfo.precedingDaysForReminder}>
+                <StyledSelect
+                    onChange={(event) => handlePrecedingDayChange(event)}
+                    defaultValue={getDefaultReminderInterval(birthdayInfo)}
+                >
+                    <MenuItem value={-1}>---</MenuItem>
                     <MenuItem value={0}>Day of</MenuItem>
                     <MenuItem value={1}>1 Day</MenuItem>
                     <MenuItem value={3}>3 Days</MenuItem>
